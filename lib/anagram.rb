312d257d90b5482ac34e04f3fd8703e058413f94
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-03-04 04:01:40 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,19 +9,20 @@ class Anagram
   def match(words)
     word_hash = to_hash(self.word)
     words.select{|test|hash_equality(word_hash, to_hash(test))}
-
   end
+
+
   private
-def to_hash(word)
-  chars = word.split('')
-  hash = {}
-  chars.each{|c| !!hash[c] ? hash[c] += 1 : hash[c] = 1}
-  hash
+  def to_hash(word)
+    chars = word.split('')
+    hash = {}
+    chars.each{|c| !!hash[c] ? hash[c] += 1 : hash[c] = 1}
+    hash
+  end
+
+  def hash_equality(hash1, hash2)
+    (hash1.keys.length == hash2.keys.length) && hash1.keys.all?{|k| hash1[k] == hash2[k]}
+  end
 end
 
-def hash_equality(hash1, hash2)
-  (hash1.keys.length == hash2.keys.length) && hash1.keys.all?{|k| hash1[k] == hash2[k]}
-end
-end
 
-end
